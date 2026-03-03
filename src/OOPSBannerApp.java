41363f9eb4c3a472b64f6f30cdda92c737a108e9
officialragesh <rv4102@srmist.edu.in>
2026-03-03 10:33:58 +0530

    UC8: Finalized architecture using HashMap registry and dynamic rendering function

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,20 +1,21 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class OOPSBannerApp {
+public class OOPSBannerApp{
 
+    
     private static final Map<Character, String[]> patternRegistry = new HashMap<>();
 
     static {
-
-        patternRegistry.put('O', new String[] {
-                " OOO ", "O   O", "O   O", "O   O", "O   O", "O   O", " OOO "
+        
+        patternRegistry.put('O', new String[]{
+            " OOO ", "O   O", "O   O", "O   O", "O   O", "O   O", " OOO "
         });
-        patternRegistry.put('P', new String[] {
-                "PPPP ", "P   P", "P   P", "PPPP ", "P    ", "P    ", "P    "
+        patternRegistry.put('P', new String[]{
+            "PPPP ", "P   P", "P   P", "PPPP ", "P    ", "P    ", "P    "
         });
-        patternRegistry.put('S', new String[] {
-                " SSS ", "S    ", "S    ", " SSS ", "    S", "    S", "SSSS "
+        patternRegistry.put('S', new String[]{
+            " SSS ", "S    ", "S    ", " SSS ", "    S", "    S", "SSSS "
         });
     }
 
@@ -23,14 +24,15 @@ public class OOPSBannerApp {
         renderBanner(word);
     }
 
+    
     public static void renderBanner(String word) {
         for (int row = 0; row < 7; row++) {
             StringBuilder line = new StringBuilder();
-
+            
             for (char ch : word.toCharArray()) {
-
+                
                 String[] pattern = patternRegistry.get(Character.toUpperCase(ch));
-
+                
                 if (pattern != null) {
                     line.append(pattern[row]).append("  ");
                 }
